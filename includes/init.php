16508fa6cd759e5e525c89b6364aedbbf1a3263a
Razon Komar Pal <raazon@users.noreply.github.com>
2024-10-19 04:49:09 +0600

    Improve: plugin main file

--- includes/init.php
+++ includes/init.php
@@ -1,15 +1,186 @@
 <?php defined('ABSPATH') || die('Cheatin&#8217; uh?'); // Cannot access pages directly.
 
-/**
- * Loading plugin functions
- * @since 1.0
- * @author Razon Komar Pal
- */
-require_once plugin_dir_path(__FILE__) . 'class-functions.php';
+final class Init
+{
+	private static $instance = null;
 
-/**
- * Loading plugin upgrade
- * @since 1.0
- * @author Razon Komar Pal
- */
-require_once plugin_dir_path(__FILE__) . 'upgrade.php';
+	private function __construct()
+	{
+		// Prevent direct instantiation.
+	}
+
+	public static function getInstance()
+	{
+		if (self::$instance === null) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	public function run()
+	{
+		// Fired when activate.
+		register_activation_hook(LVFW_FILE, [__CLASS__, 'activate']);
+
+		// Fired when deactivate.
+		register_deactivation_hook(LVFW_FILE, [__CLASS__, 'deactivate']);
+
+		// Load plugin textdomain.
+		add_action('init', [__CLASS__, 'load_textdomain']);
+
+		// Admin notice if Woocommerce is not installed.
+		add_action('admin_notices', [__CLASS__, 'admin_notice']);
+	}
+
+	public static function activate() {}
+
+	public static function deactivate() {}
+
+	public static function load_textdomain()
+	{
+		load_plugin_textdomain(
+			'linked-variation-for-woocommerce',
+			false,
+			LVFW_PATH . '/languages'
+		);
+	}
+
+	public static function admin_noticeX()
+	{
+		if (!current_user_can('activate_plugins') || class_exists('WooCommerce')) {
+			return;
+		}
+
+		$plugin_path = 'woocommerce/woocommerce.php'; // Path to the WooCommerce plugin file.
+		$installed_plugins = get_plugins();
+		$active_plugins = get_option('active_plugins');
+
+		// class_exists('WooCommerce') || !in_array($plugin_path, $active_plugins)
+
+		if (!isset($installed_plugins[$plugin_path])) {
+			$notice_html = sprintf(
+				/* translators: 1: Plugin name 2: WooCommerce */
+				esc_html__('The %1$s plugin requires %2$s to be installed and activated.', 'linked-variation-for-woocommerce'),
+				'<strong>' . esc_html__('Linked Variation for WooCommerce', 'linked-variation-for-woocommerce') . '</strong>',
+				'<strong>' . esc_html__('WooCommerce', 'linked-variation-for-woocommerce') . '</strong>'
+			);
+
+			$install_url = wp_nonce_url(
+				self_admin_url('update.php?action=install-plugin&plugin=woocommerce'),
+				'install-plugin_woocommerce'
+			);
+
+			$notice_html .= sprintf(
+				'<br><a href="%s" class="button-primary" style="margin-top: 5px;">%s</a>',
+				esc_url($install_url),
+				esc_html__('Install WooCommerce', 'linked-variation-for-woocommerce')
+			);
+		}
+
+		if (!in_array($plugin_path, $active_plugins)) {
+			$notice_html = sprintf(
+				/* translators: 1: Plugin name 2: WooCommerce */
+				esc_html__('The %1$s plugin requires %2$s to be activated.', 'linked-variation-for-woocommerce'),
+				'<strong>' . esc_html__('Linked Variation for WooCommerce', 'linked-variation-for-woocommerce') . '</strong>',
+				'<strong>' . esc_html__('WooCommerce', 'linked-variation-for-woocommerce') . '</strong>'
+			);
+
+			$activate_url = wp_nonce_url(
+				add_query_arg(
+					[
+						'action' => 'activate',
+						'plugin' => $plugin_path,
+					],
+					admin_url('plugins.php')
+				),
+				'activate-plugin_' . $plugin_path
+			);
+
+			$notice_html .= sprintf(
+				'<br><a href="%s" class="button-primary" style="margin-top: 5px;">%s</a>',
+				$activate_url,
+				esc_html__('Activate WooCommerce', 'linked-variation-for-woocommerce')
+			);
+		}
+
+		if (empty($notice_html)) {
+			return;
+		}
+
+		printf(
+			'<div class="notice notice-error is-dismissible"><p>%s</p></div>',
+			$notice_html
+		);
+	}
+
+	public static function admin_notice()
+	{
+		if (!current_user_can('activate_plugins') || class_exists('WooCommerce')) {
+			return;
+		}
+
+		$plugin_path = 'woocommerce/woocommerce.php'; // Path to the WooCommerce plugin file.
+		$installed_plugins = get_plugins();
+		$active_plugins = get_option('active_plugins');
+		$notice_html = '';
+
+		// Check if WooCommerce is installed.
+		if (!isset($installed_plugins[$plugin_path])) {
+			$install_url = wp_nonce_url(
+				self_admin_url('update.php?action=install-plugin&plugin=woocommerce'),
+				'install-plugin_woocommerce'
+			);
+
+			$notice_html = sprintf(
+				/* translators: 1: Plugin name 2: WooCommerce */
+				esc_html__('The %1$s plugin requires %2$s to be installed and activated.', 'linked-variation-for-woocommerce'),
+				'<strong>' . esc_html__('Linked Variation for WooCommerce', 'linked-variation-for-woocommerce') . '</strong>',
+				'<strong>' . esc_html__('WooCommerce', 'linked-variation-for-woocommerce') . '</strong>'
+			);
+
+			$notice_html .= sprintf(
+				'<br><a href="%s" class="button-primary" style="margin-top: 5px;">%s</a>',
+				esc_url($install_url),
+				esc_html__('Install WooCommerce', 'linked-variation-for-woocommerce')
+			);
+		}
+
+		// Check if WooCommerce is installed but not activated.
+		if (isset($installed_plugins[$plugin_path]) && !in_array($plugin_path, $active_plugins)) {
+			$activate_url = wp_nonce_url(
+				add_query_arg(
+					[
+						'action' => 'activate',
+						'plugin' => $plugin_path,
+					],
+					admin_url('plugins.php')
+				),
+				'activate-plugin_' . $plugin_path
+			);
+
+			$notice_html = sprintf(
+				/* translators: 1: Plugin name 2: WooCommerce */
+				esc_html__('The %1$s plugin requires %2$s to be activated.', 'linked-variation-for-woocommerce'),
+				'<strong>' . esc_html__('Linked Variation for WooCommerce', 'linked-variation-for-woocommerce') . '</strong>',
+				'<strong>' . esc_html__('WooCommerce', 'linked-variation-for-woocommerce') . '</strong>'
+			);
+
+			$notice_html .= sprintf(
+				'<br><a href="%s" class="button-primary" style="margin-top: 5px;">%s</a>',
+				esc_url($activate_url),
+				esc_html__('Activate WooCommerce', 'linked-variation-for-woocommerce')
+			);
+		}
+
+		// Display the notice if needed.
+		if (!empty($notice_html)) {
+			printf(
+				'<div class="notice notice-error is-dismissible"><p>%s</p></div>',
+				$notice_html
+			);
+		}
+	}
+}
+
+// Get the singleton instance and run the plugin.
+Init::getInstance()->run();
--- linked-variation-for-woocommerce.php
+++ linked-variation-for-woocommerce.php
@@ -1,17 +1,12 @@
 <?php
 
 /**
- *
- * @link              https://devsace.com/
- * @package           WooLinkedVariation
- *
- * @wordpress-plugin
  * Plugin Name:       Linked Variation for WooCommerce
- * Plugin URI:        https://demos.devsace.com/plugins/linked-variation-for-woocommerce/
+ * Plugin URI:        https://raazon.com/
  * Description:       This is a helper plugin of WooCommerce built to link separate products together by attributes.
- * Version:           1.0.4
- * Author:            DevsAce
- * Author URI:        https://devsace.com/
+ * Version:           2.0.0
+ * Author:            Razon
+ * Author URI:        https://raazon.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       linked-variation-for-woocommerce
@@ -21,43 +16,19 @@
  * WC tested up to: 5.3.0
  */
 
-if (!defined('ABSPATH')) exit; // Exit if accessed directly
-
-// load once plugin.php file
-if (!function_exists('is_plugin_active')) {
-    include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+// Exit if accessed directly.
+if (! defined('ABSPATH')) {
+	exit;
 }
 
-// Load plugin textdomain.
-add_action('init', 'woo_linked_variation_load_textdomain');
-function woo_linked_variation_load_textdomain()
-{
-    load_plugin_textdomain('linked-variation-for-woocommerce', false, dirname(plugin_basename(__FILE__)) . '/languages/');
-}
-
-// Declering Constant
-define('LVFW_VERSION', '1.0.4'); // Currently plugin version.
-define('LVFW_API_URL', 'https://api.devsace.com/linked-variation-for-woocommerce'); // Currently plugin version.
-define('LVFW_FILE', __FILE__);
+// Define plugin constants.
+define('LVFW_VERSION', '2.0.0');
+define('LVFW_FILE', trailingslashit(__FILE__));
 define('LVFW_BASENAME', plugin_basename(__FILE__));
-define('LVFW_PATH', plugin_dir_path(__FILE__));
-define('LVFW_INCLUDE_PATH', trailingslashit(LVFW_PATH) . 'includes/');
-
-// The code that runs during plugin activation.
-function activate_woo_linked_variation()
-{
-    require_once LVFW_PATH . '/includes/class-activator.php';
-}
-register_activation_hook(__FILE__, 'activate_woo_linked_variation');
-
-
-// The code that runs during plugin deactivation.
-function deactivate_woo_linked_variation()
-{
-    require_once LVFW_PATH . '/includes/class-deactivator.php';
-}
-register_deactivation_hook(__FILE__, 'deactivate_woo_linked_variation');
-
+define('LVFW_PATH', trailingslashit(plugin_dir_path(__FILE__)));
+define('LVFW_URL', trailingslashit(plugin_dir_url(__FILE__)));
+define('LVFW_INCLUDE_PATH', LVFW_PATH . 'includes/');
+define('LVFW_API_URL', 'https://api.devsace.com/linked-variation-for-woocommerce');
 
 // Loading init file
 require_once LVFW_PATH . '/includes/init.php';

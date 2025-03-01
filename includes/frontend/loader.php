64f230b7f7dc30b42c18a62bbd133e0466234260
Razon Komar Pal <raazon@users.noreply.github.com>
2025-03-02 02:21:37 +0600

    Create loader.php

---
+++ includes/frontend/loader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Load frontend files.
+ *
+ * @package Lvfw
+ * @since 2.0.0
+ */
+
+defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+
+require_once LVFW_INCLUDE_PATH . 'frontend/display.php';

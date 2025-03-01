675bac97c177457b7c75c9534deb627b9c56ea40
Razon Komar Pal <raazon@users.noreply.github.com>
2025-03-01 22:46:48 +0600

    Create loader.php

---
+++ includes/core/loader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Load core files.
+ *
+ * @package Lvfw
+ * @since 2.0.0
+ */
+
+defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+
+// require_once LVFW_INCLUDE_PATH . 'core/cpt.php';

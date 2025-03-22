f142f7be81e92aba0f51028be75b3bc01ef5ef73
Razon Komar Pal <raazon@users.noreply.github.com>
2025-03-22 22:15:41 +0600

    Update ajax.php

--- includes/admin/ajax.php
+++ includes/admin/ajax.php
@@ -1,59 +1,59 @@
 <?php defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' ); // Cannot access pages directly.
 
-// Customize the callback to your liking
+// Customize the callback to your liking.
 function lvfw_get_source_products() {
 	$search_term = isset( $_REQUEST['search'] ) ? esc_attr( $_REQUEST['search'] ) : '';
 
-	// Query WooCommerce products
+	// Query WooCommerce products.
 	$args = array(
-		'limit'   => 10, // Limit the number of results
-		'status'  => 'publish', // Only include published products
+		'limit'   => 10, // Limit the number of results.
+		'status'  => 'publish', // Only include published products.
 		'orderby' => 'title',
 		'order'   => 'ASC',
 	);
 
-	// Include a search query if provided
+	// Include a search query if provided.
 	if ( ! empty( $search_term ) ) {
-		$args['s'] = $search_term; // Search by product title
+		$args['s'] = $search_term; // Search by product title.
 	}
 
-	// Use WooCommerce Product Query
+	// Use WooCommerce Product Query.
 	$product_query = new WC_Product_Query( $args );
 	$products      = $product_query->get_products();
 
-	// Format the results for Select2
+	// Format the results for Select2.
 	$results = array();
 	foreach ( $products as $product ) {
 		$results[] = array(
-			'id'   => $product->get_id(), // Product ID
-			'text' => $product->get_name(), // Product Name
+			'id'   => $product->get_id(), // Product ID.
+			'text' => $product->get_name(), // Product Name.
 		);
 	}
 
-	// Return the results as JSON
+	// Return the results as JSON.
 	echo wp_json_encode( $results );
 
-	wp_die(); // Required to terminate AJAX calls properly
+	wp_die(); // Required to terminate AJAX calls properly.
 }
 
 add_action( 'wp_ajax_lvfw_get_source_products', 'lvfw_get_source_products' );
 
-// Customize the callback to your liking
+// Customize the callback to your liking.
 function lvfw_get_source_taxonomy() {
 	$taxonomy    = isset( $_REQUEST['taxonomy'] ) ? esc_attr( $_REQUEST['taxonomy'] ) : '';
 	$search_term = isset( $_REQUEST['search'] ) ? esc_attr( $_REQUEST['search'] ) : '';
 
-	// Query WooCommerce products categories taxonomy
+	// Query WooCommerce products categories taxonomy.
 	$taxonomies = get_terms(
 		array(
 			'taxonomy'   => $taxonomy,
 			'hide_empty' => true,
-			'name__like' => $search_term, // Search term
-			'number'     => 10, // Limit results
+			'name__like' => $search_term, // Search term.
+			'number'     => 10, // Limit results.
 		)
 	);
 
-	// Format the results for Select2
+	// Format the results for Select2.
 	$results = array();
 	foreach ( $taxonomies as $taxonomy ) {
 		$results[] = array(
@@ -62,27 +62,27 @@ function lvfw_get_source_taxonomy() {
 		);
 	}
 
-	// Return the results as JSON
+	// Return the results as JSON.
 	echo wp_json_encode( $results );
 
-	wp_die(); // Required to terminate AJAX calls properly
+	wp_die(); // Required to terminate AJAX calls properly.
 }
 
 add_action( 'wp_ajax_lvfw_get_source_taxonomy', 'lvfw_get_source_taxonomy' );
 
-
+// Handles the AJAX request to retrieve a new variation form.
 function lvfw_get_new_variation() {
-	$key = isset( $_REQUEST['key'] ) ? esc_attr( $_REQUEST['key'] ) : '';
+	$key = isset( $_REQUEST['key'] ) ? esc_attr( wp_unslash( $_REQUEST['key'] ) ) : '';
 
-	// Get product attributes
+	// Get product attributes.
 	$product_attributes = wc_get_attribute_taxonomies();
 
-	// Output the new variation form
+	// Output the new variation form.
 	ob_start();
 	include LVFW_INCLUDE_PATH . 'admin/new-variation.php';
 	$output = ob_get_clean();
 
-	// Return the output
+	// Return the output.
 	echo wp_json_encode(
 		array(
 			'key'    => $key + 1,
@@ -90,7 +90,7 @@ function lvfw_get_new_variation() {
 		)
 	);
 
-	wp_die(); // Required to terminate AJAX calls properly
+	wp_die(); // Required to terminate AJAX calls properly.
 }
 
 add_action( 'wp_ajax_lvfw_get_new_variation', 'lvfw_get_new_variation' );

64c2873c18e0e298c014d9b23620915572feb0c6
Razon Komar Pal <raazon@users.noreply.github.com>
2025-05-09 22:56:51 +0600

    Update cpt.php

--- includes/admin/cpt.php
+++ includes/admin/cpt.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Register Custom Post Type.
  *
@@ -6,7 +7,7 @@
  * @since 2.0.0
  */
 
-defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+defined('ABSPATH') || die('Cheatin&#8217; uh?');
 
 /**
  * Register Custom Post Type.
@@ -14,43 +15,44 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
  * @since 2.0.0
  * @package Lvfw
  */
-function lvfw_create_woolinkedvariation_cpt() {
+function lvfw_create_woolinkedvariation_cpt()
+{
 	$labels = array(
-		'name'                  => _x( 'Linked Variations', 'Post Type General Name', 'linked-variation-for-woocommerce' ),
-		'singular_name'         => _x( 'Linked Variation', 'Post Type Singular Name', 'linked-variation-for-woocommerce' ),
-		'menu_name'             => _x( 'Linked Variations', 'Admin Menu text', 'linked-variation-for-woocommerce' ),
-		'name_admin_bar'        => _x( 'Linked Variation', 'Add New on Toolbar', 'linked-variation-for-woocommerce' ),
-		'archives'              => esc_html__( 'Linked Variation Archives', 'linked-variation-for-woocommerce' ),
-		'attributes'            => esc_html__( 'Linked Variation Attributes', 'linked-variation-for-woocommerce' ),
-		'parent_item_colon'     => esc_html__( 'Parent Linked Variation:', 'linked-variation-for-woocommerce' ),
-		'all_items'             => esc_html__( 'Linked Variations', 'linked-variation-for-woocommerce' ),
-		'add_new_item'          => esc_html__( 'Add New Linked Variation', 'linked-variation-for-woocommerce' ),
-		'add_new'               => esc_html__( 'Add New Linked Variation', 'linked-variation-for-woocommerce' ),
-		'new_item'              => esc_html__( 'New Linked Variation', 'linked-variation-for-woocommerce' ),
-		'edit_item'             => esc_html__( 'Edit Linked Variation', 'linked-variation-for-woocommerce' ),
-		'update_item'           => esc_html__( 'Update Linked Variation', 'linked-variation-for-woocommerce' ),
-		'view_item'             => esc_html__( 'View Linked Variation', 'linked-variation-for-woocommerce' ),
-		'view_items'            => esc_html__( 'View Linked Variations', 'linked-variation-for-woocommerce' ),
-		'search_items'          => esc_html__( 'Search Linked Variation', 'linked-variation-for-woocommerce' ),
-		'not_found'             => esc_html__( 'No linked variations found', 'linked-variation-for-woocommerce' ),
-		'not_found_in_trash'    => esc_html__( 'Not found in Trash', 'linked-variation-for-woocommerce' ),
-		'featured_image'        => esc_html__( 'Featured Image', 'linked-variation-for-woocommerce' ),
-		'set_featured_image'    => esc_html__( 'Set featured image', 'linked-variation-for-woocommerce' ),
-		'remove_featured_image' => esc_html__( 'Remove featured image', 'linked-variation-for-woocommerce' ),
-		'use_featured_image'    => esc_html__( 'Use as featured image', 'linked-variation-for-woocommerce' ),
-		'insert_into_item'      => esc_html__( 'Insert into Linked Variation', 'linked-variation-for-woocommerce' ),
-		'uploaded_to_this_item' => esc_html__( 'Uploaded to this Linked Variation', 'linked-variation-for-woocommerce' ),
-		'items_list'            => esc_html__( 'Linked Variations list', 'linked-variation-for-woocommerce' ),
-		'items_list_navigation' => esc_html__( 'Linked Variations list navigation', 'linked-variation-for-woocommerce' ),
-		'filter_items_list'     => esc_html__( 'Filter Linked Variations list', 'linked-variation-for-woocommerce' ),
+		'name'                  => _x('Linked Variations', 'Post Type General Name', 'linked-variation-for-woocommerce'),
+		'singular_name'         => _x('Linked Variation', 'Post Type Singular Name', 'linked-variation-for-woocommerce'),
+		'menu_name'             => _x('Linked Variations', 'Admin Menu text', 'linked-variation-for-woocommerce'),
+		'name_admin_bar'        => _x('Linked Variation', 'Add New on Toolbar', 'linked-variation-for-woocommerce'),
+		'archives'              => esc_html__('Linked Variation Archives', 'linked-variation-for-woocommerce'),
+		'attributes'            => esc_html__('Linked Variation Attributes', 'linked-variation-for-woocommerce'),
+		'parent_item_colon'     => esc_html__('Parent Linked Variation:', 'linked-variation-for-woocommerce'),
+		'all_items'             => esc_html__('Linked Variations', 'linked-variation-for-woocommerce'),
+		'add_new_item'          => esc_html__('Add New Linked Variation', 'linked-variation-for-woocommerce'),
+		'add_new'               => esc_html__('Add New Linked Variation', 'linked-variation-for-woocommerce'),
+		'new_item'              => esc_html__('New Linked Variation', 'linked-variation-for-woocommerce'),
+		'edit_item'             => esc_html__('Edit Linked Variation', 'linked-variation-for-woocommerce'),
+		'update_item'           => esc_html__('Update Linked Variation', 'linked-variation-for-woocommerce'),
+		'view_item'             => esc_html__('View Linked Variation', 'linked-variation-for-woocommerce'),
+		'view_items'            => esc_html__('View Linked Variations', 'linked-variation-for-woocommerce'),
+		'search_items'          => esc_html__('Search Linked Variation', 'linked-variation-for-woocommerce'),
+		'not_found'             => esc_html__('No linked variations found', 'linked-variation-for-woocommerce'),
+		'not_found_in_trash'    => esc_html__('Not found in Trash', 'linked-variation-for-woocommerce'),
+		'featured_image'        => esc_html__('Featured Image', 'linked-variation-for-woocommerce'),
+		'set_featured_image'    => esc_html__('Set featured image', 'linked-variation-for-woocommerce'),
+		'remove_featured_image' => esc_html__('Remove featured image', 'linked-variation-for-woocommerce'),
+		'use_featured_image'    => esc_html__('Use as featured image', 'linked-variation-for-woocommerce'),
+		'insert_into_item'      => esc_html__('Insert into Linked Variation', 'linked-variation-for-woocommerce'),
+		'uploaded_to_this_item' => esc_html__('Uploaded to this Linked Variation', 'linked-variation-for-woocommerce'),
+		'items_list'            => esc_html__('Linked Variations list', 'linked-variation-for-woocommerce'),
+		'items_list_navigation' => esc_html__('Linked Variations list navigation', 'linked-variation-for-woocommerce'),
+		'filter_items_list'     => esc_html__('Filter Linked Variations list', 'linked-variation-for-woocommerce'),
 	);
 
 	$args = array(
-		'label'               => esc_html__( 'Linked Variation', 'linked-variation-for-woocommerce' ),
-		'description'         => esc_html__( 'WooCommerce Linked Variations', 'linked-variation-for-woocommerce' ),
+		'label'               => esc_html__('Linked Variation', 'linked-variation-for-woocommerce'),
+		'description'         => esc_html__('WooCommerce Linked Variations', 'linked-variation-for-woocommerce'),
 		'labels'              => $labels,
 		'menu_icon'           => 'dashicons-admin-links',
-		'supports'            => array( 'title', 'revisions' ),
+		'supports'            => array('title', 'revisions'),
 		'taxonomies'          => array(),
 		'public'              => false,
 		'show_ui'             => true,
@@ -67,6 +69,6 @@ function lvfw_create_woolinkedvariation_cpt() {
 		'capability_type'     => 'post',
 	);
 
-	register_post_type( 'woolinkedvariation', $args );
+	register_post_type('woolinkedvariation', $args);
 }
-add_action( 'init', 'lvfw_create_woolinkedvariation_cpt', 10, 1 );
+add_action('init', 'lvfw_create_woolinkedvariation_cpt', 10, 1);
